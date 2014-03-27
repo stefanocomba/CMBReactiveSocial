a1da1e4e7b286d722f3bba6acef86f2f8617f368
Stefano Comba <stefano.comba@gmail.com>
2014-03-27 10:04:33 +0100

    first commit

---
+++ SCReactiveSocialExample/SCReactiveSocialExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SCReactiveSocialExample
+//
+//  Created by Stefano Comba on 27/03/14.
+//  Copyright (c) 2014 Stefano Comba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
